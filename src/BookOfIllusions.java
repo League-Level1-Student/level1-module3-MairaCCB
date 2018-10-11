c2af17e63d5f55c43f0198893ad532c1d0d8df29
Anjruski <league@WTS10.attlocal.net>
2018-10-10 18:24:23 -0700

    TypingTutor, BookOfIllusions(IP)

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -12,14 +12,18 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
- 
-/** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
+
+/**
+ * We’re going to make a slideshow of cool optical illusions. When the user
+ * clicks on an illusion, a new one will be loaded.
+ **/
 
 public class BookOfIllusions extends MouseAdapter {
 
 	/*
-	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
-	 * static main method, so that we can add a click listener to each illusion.
+	 * Here we are instantiating our BookOfIllusions class and calling it’s
+	 * createBook() method. This is because we want to get out of the static main
+	 * method, so that we can add a click listener to each illusion.
 	 */
 
 	public static void main(String[] args) throws Exception {
@@ -29,29 +33,44 @@ public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
 	JFrame frame = new JFrame();
+
 	private void createBook() {
 		// 2. make the frame visible
+		frame.setVisible(true);
 		// 3. set the size of the frame
+		frame.setSize(500, 500);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String corgi = "Corgi2.jpg";
+		String puppy = "Puppy2.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		JLabel label;
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		label = new JLabel();
+		loadImageFromComputer("Corgi2.jpg");
 		// 8. add your JLabel to the frame
+		label.add(frame);
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
-		// 11. Print "clicked!" to the console when the mouse is pressed
+		// 11. Print "Clicked!" to the console when the mouse is pressed
+		System.out.println("Clicked!");
 		// 12. remove everything from the frame that was added earlier
+		frame.remove(frame);
 		// 13. load a new image like before (this is more than one line of code)
+		
 		// 14. pack the frame
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
 
 	/*
-	 * To use this method, the image must be placed in your Eclipse project under "default package".
+	 * To use this method, the image must be placed in your Eclipse project under
+	 * "default package".
 	 */
 	public JLabel loadImageFromComputer(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
@@ -60,5 +79,3 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 }
-
-
