c2f38b817e933fcb5a11326415c447945f81d25e
League <league@WTS9.attlocal.net>
2018-10-29 16:55:42 -0700

    Calculator(IP)

--- src/extra/CalculatorRunner.java
+++ src/extra/CalculatorRunner.java
@@ -6,25 +6,42 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 
 public class CalculatorRunner implements ActionListener {
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	JTextField textPane1 = new JTextField(10);
+	JTextField textPane2 = new JTextField(10);
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
 	JButton button3 = new JButton();
 	JButton button4 = new JButton();
 	
-
+public static void main(String[] args) {
+	new CalculatorRunner();
+}
 	public CalculatorRunner() {
 		// TODO Auto-generated constructor stub
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Simple Calculator");
-		frame.add(label);
-		label.add(button1);
-		label.add(button2);
-		label.add(button3);
-		label.add(button4);
+		frame.setSize(350, 300);
+		frame.add(panel);
+		panel.add(textPane1);
+		panel.add(textPane2);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		panel.add(label);
+		button1.setText("add");
+		button2.setText("sub");
+		button3.setText("mul");
+		button4.setText("div");
 
 	}
 
@@ -32,11 +49,9 @@ public class CalculatorRunner implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		button4.addActionListener(this);
 	}
 }
-
-//button1.addActionListener(this);
-//button2.addActionListener(this);
-//button3.addActionListener(this);
-//button4.addActionListener(this);
