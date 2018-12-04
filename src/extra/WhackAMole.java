857664600f03a62a842e3b2d15a4bd4b7e6f593f
League Student <league@iMac-4.attlocal.net>
2018-12-03 17:00:11 -0800

    WhackAMole(IP)

--- src/extra/WhackAMole.java
+++ src/extra/WhackAMole.java
@@ -1,31 +1,60 @@
 package extra;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	
-	
+
 	public static void main(String[] args) {
-		new WhackAMole();
+		WhackAMole w = new WhackAMole();
+		w.drawButtons();
+	}
+
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public WhackAMole() {
+	Random r = new Random();
+	int a;
+	public void drawButtons() {
+		a = r.nextInt(24);
+
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Whack a Button for Fame and Glory");
 		frame.add(panel);
-		frame.setSize(500, 500);
-		
-		for(int i = 0; i < 24; i++) {
+		frame.setSize(295, 300);
+
+		for (int i = 0; i < 24; i++) {
 			JButton button = new JButton();
 			panel.add(button);
+			button.addActionListener(this);
+
+			if (a == i) {
+				button.setText("mole!");
+			}
 		}
-		
-		
-		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed.getText() == "mole!") {
+			speak("you missed");
+			frame.dispose();
+			WhackAMole m = new WhackAMole();
+			m.drawButtons();
+		}
 	}
 }
