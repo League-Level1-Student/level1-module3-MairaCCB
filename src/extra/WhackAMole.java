de98946888d71d8c84f0e024ada2534041071d7d
MairaCCB <league@iMac-5.attlocal.net>
2018-12-10 16:55:32 -0800

    WhackAMole

--- src/extra/WhackAMole.java
+++ src/extra/WhackAMole.java
@@ -1,20 +1,25 @@
-package extra;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	static Date d;
+	static int count = 0;
+	static int miss = 0;
 
 	public static void main(String[] args) {
 		WhackAMole w = new WhackAMole();
 		w.drawButtons();
+		d = new Date();
 	}
 
 	void speak(String words) {
@@ -24,9 +29,10 @@ public class WhackAMole implements ActionListener {
 			e.printStackTrace();
 		}
 	}
-	
+
 	Random r = new Random();
 	int a;
+
 	public void drawButtons() {
 		a = r.nextInt(24);
 
@@ -46,15 +52,38 @@ public class WhackAMole implements ActionListener {
 			}
 		}
 	}
+
+	private void endGame(Date timeAtStart, int molesWhacked) {
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+				+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		JButton buttonPressed = (JButton) e.getSource();
+		WhackAMole m = new WhackAMole();
+
 		if (buttonPressed.getText() == "mole!") {
-			speak("you missed");
+			count++;
 			frame.dispose();
-			WhackAMole m = new WhackAMole();
+			m.drawButtons();
+
+		} else {
+			speak("you missed");
+			miss++;
+			frame.dispose();
 			m.drawButtons();
 		}
+		if (count > 10) {
+			m.endGame(d, count);
+		}
+
+		else if (miss >= 5) {
+			speak("Sorry, you lost.");
+			m.endGame(d, miss);
+		}
+
 	}
 }
