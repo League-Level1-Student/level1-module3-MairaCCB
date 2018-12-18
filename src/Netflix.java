47266716c41b1f3b5e9516ac9897674c5f4b2c9f
League Student <league@iMac-4.attlocal.net>
2018-12-17 16:56:02 -0800

    netflix(IP)

--- src/Netflix.java
+++ src/Netflix.java
@@ -1,6 +1,10 @@
 
 public class Netflix {
 	public static void main(String[] args) {
-
+		Movie m1 = new Movie("", 0);
+		Movie m2 = new Movie("", 0);
+		Movie m3 = new Movie("", 0);
+		Movie m4 = new Movie("", 0);
+		Movie m5 = new Movie("", 0);
 	}
 }
